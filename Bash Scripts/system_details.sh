5967a99379ebc8de3a98315d7ae13d72af157594
Pavan Sadanala <pavansadanala77@gmail.com>
2025-11-27 04:02:58 +0530

    Update system_details.sh

--- Bash Scripts/system_details.sh
+++ Bash Scripts/system_details.sh
@@ -3,3 +3,7 @@
 echo "Getting Hostname.............."
 Host=$(hostname)
 echo "Hostname is $Host"
+echo 'Getting kernel Info.................'
+kernelInfo=$(uname -r)
+echo "Kernel Info - $kernelInfo"
+echo -n "uptime of the server is $(uptime)"
